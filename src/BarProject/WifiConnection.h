02c74d8c238908ea00f5f5312a1a832d7cc8bcd5
Alejandro Alvarado <alejalvarado@paypal.com>
2017-04-21 23:39:35 -0600

    Esp8266 Wifi lib

--- src/BarProject/WifiConnection.h
+++ src/BarProject/WifiConnection.h
@@ -2,7 +2,13 @@
 #define MYARDUINOPROJECT_WIFI_CLIENT_H
 
 #include <SPI.h>
+#include <SoftwareSerial.h> 
+#include "ESP8266/SparkFunESP8266WiFi.h"
 
+//TO-DO: probably will want to make this an interface and extract implementation
+//details to subclass ESP8266WifiConnection.
+//**OR** leave the class WifiConnection and just create an interface that acts as 
+//a wrapper for the ESP8266 (or any other wifi module)
 class WifiConnection {
 
 public:
@@ -12,8 +18,13 @@ public:
 
 private:
 	
-	char ssid[17] = "TURBONETT_0B2C35";
-	char pass[13] = "g@t0sc0nw1f1";		
+	const char *ssid = "wonderland";
+	const char *psk = "starstuff";	
+	//ESP8266Class esp8266;
+
+	void initializeESP8266();	
+	void connectESP8266();
+	void displayConnectInfo();
 };
 
 #endif
